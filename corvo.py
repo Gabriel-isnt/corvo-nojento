84226d0613b0315675da120c93a5357ae376b2a7
At_oa <85297718+Gabriel-isnt@users.noreply.github.com>
2023-09-15 18:40:01 -0300

    corvo.py

--- corvo.py
+++ corvo.py
@@ -1,10 +1,12 @@
 def binario_para_inteiro(sequencia):
     if '*' in sequencia or '-' in sequencia: 
+        # vejo se tem '*' na sequencia e depois eu vejo se tem '-'
+        # se eu fisesse """"('*' or '-') in sequencia"""" quando um desse verdade ele não procuraria pelo outro
         sequencia = sequencia.replace('*', '1')
         sequencia = sequencia.replace('-', '0')
 
     return int(sequencia, 2)
-
+    # esse 2 faz o int entender que é um binário e passa ele para um inteiro
 
 lista_normal = []
 lista_somada = []
@@ -13,7 +15,7 @@ conta_canto = 0
 
 while conta_canto != 3:
     print('escreva "caw" para o corvo somar')
-    acao_corvo = str(input('coloque a sequencia do corvo: ').strip())
+    acao_corvo = input('coloque a sequencia do corvo: ').strip()
     
     if acao_corvo != 'caw':
         numero_corvo = binario_para_inteiro(acao_corvo)
